30ce221c736b00e1a830a1206bbcccf0362e9aec
James Burke <james@armifi.com>
2015-07-27 13:32:50 -0700

    Added bell when a new post is written to the term

--- pyred.py
+++ pyred.py
@@ -1,31 +1,33 @@
-#! /usr/bin/python
+    #! /usr/bin/python
 # author: /u/rasputine
 import urllib2
 from sys import stdout
 from time import sleep
 import json
 import argparse
+from datetime import datetime
 import sys
 import os
 parser = argparse.ArgumentParser(description="Prints out the newest post from a subreddit.")
 parser.add_argument('subreddit', metavar='C', type=str, nargs='*',
-	help="Specify the subreddit to follow")
+    help="Specify the subreddit to follow")
 parser.add_argument('-s','--not_secure', dest='secure', action='store_const',
-	const='http://', default='https://', help='Flag to disable https, defaults to https.')
+    const='http://', default='https://', help='Flag to disable https, defaults to https.')
 parser.add_argument('-1','--once', dest='repeat', action='store_const',
-	const=False, default=True, help='Flag to only run once')
+    const=False, default=True, help='Flag to only run once')
 parser.add_argument('-u', '--user', dest='username', default=None)
 parser.add_argument('-f', '--feed', dest='feed', default=None)
 args = vars(parser.parse_args())
+delay=2
 
 secure=args['secure']
 subreddit=args['subreddit'][0]
 
 if args['feed']:
-	feed=str("&feed="+args['feed'])
+    feed=str("&feed="+args['feed'])
 else: feed=""
 if args['username']:
-	username=str("&user="+args['username'])
+    username=str("&user="+args['username'])
 else: username=""
 
 
@@ -35,45 +37,50 @@ req = urllib2.Request(redditurl, None, hdr)
 temp=post=""
 
 def clear():
-	os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def termPad(input_string):
-	columns = int(os.popen('stty size', 'r').read().split()[1])
-	spaces = " "*(columns - len(input_string))
-	return u"{0}{1}".format(input_string, spaces)
+    try: 
+        columns = int(os.popen('stty size', 'r').read().split()[1])
+    except:
+        columns = 80
+    spaces = " "*(columns - len(input_string))
+    return u"{0}{1}".format(input_string, spaces)
 
-def termTrim(title, comments):
-	try: 
-		columns = int(os.popen('stty size', 'r').read().split()[1])
-	except:
-		columns = 80
-	maxLen = columns - len(comments) - 3
-	return termPad(u"\r{0} - {1}".format(title[:maxLen],comments))
+def termTrim(title, comments, age):
+    try: 
+        columns = int(os.popen('stty size', 'r').read().split()[1])
+    except:
+        columns = 80
+    maxLen = columns - len(comments) - 3
+    return termPad(u"\r{0} - {1} {2}".format(title[:maxLen],comments, age))
 clear()
 while True:
-	try:
-		feed = json.loads(urllib2.urlopen(req).read())
-	except:
-		clear()
-		stdout.write( termPad("\r Error: " + str(sys.exc_info())) )
-		stdout.flush()
-		sleep(30)
-		continue
-	try:
-		jsonpost = feed['data']['children'][0]["data"]
-	except:
-		clear()
-		stdout.write( termPad("\r - No new posts - "  ) )
-		stdout.flush()
-		sleep(2)
-		continue
-	pid = jsonpost["id"]
-	comments = "{0}www.reddit.com/{1}".format(secure, pid)
-	post = termTrim(jsonpost["title"],comments)
-	if not (temp == pid):
-		temp = pid
-		clear()
-		stdout.write( post )
-		stdout.flush()
-	if not args['repeat']: break
-	sleep(5)
+    try:
+        feed = json.loads(urllib2.urlopen(req).read())
+    except:
+        clear()
+        stdout.write( termPad("\r Error: " + str(sys.exc_info())) )
+        stdout.flush()
+        sleep(delay)
+        continue
+    try:
+        jsonpost = feed['data']['children'][0]["data"]
+    except:
+        clear()
+        stdout.write( termPad("\r - No new posts - "  ) )
+        stdout.flush()
+        sleep(delay)
+        continue
+    pid = jsonpost["id"]
+    age = datetime.now() - datetime.fromtimestamp(jsonpost["created_utc"])
+    comments = "{0}www.reddit.com/{1}".format(secure, pid)
+    post = termTrim(jsonpost["title"],comments, age)
+    if not (temp == pid):
+        temp = pid
+        clear()
+        stdout.write( post )
+        stdout.flush()
+    if not args['repeat']: break
+    sleep(delay
+    	)
